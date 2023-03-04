f8884682952d172a0c3be882ae3dde5710d7c539
Kotskie <48470252+Kotskie@users.noreply.github.com>
2023-03-04 13:44:43 +0200

    dist output changes

--- src/app/repo/card.respository.ts
+++ src/app/repo/card.respository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Card } from "../models/card";
 import { Uris } from "../uris/uris";
@@ -11,36 +11,52 @@ import { ICardRepo } from "./iCardRepo";
 export class CardRepository implements ICardRepo {
   
   private http!: HttpClient;
+  private headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
 
   constructor(private uris: Uris){}
 
   private getAll(url: string): Observable<Card[]>{
-    return this.http.get<Card[]>(url);
+    const httpOptions = {
+      headers: this.headers
+    };
+    return this.http.get<Card[]>(url, httpOptions);
   }
 
   private get(url: string): Observable<Card>{
-    return this.http.get<Card>(url);
+    const httpOptions = {
+      headers: this.headers
+    };
+    return this.http.get<Card>(url, httpOptions);
   }
 
   private put(url: string, body: {}): Observable<Card>{
-    return this.http.put<Card>(url, body);
+    const httpOptions = {
+      headers: this.headers
+    };
+    return this.http.put<Card>(url, body, httpOptions);
   }
 
   private post(url: string, body: {}): Observable<Card>{
-    return this.http.post<Card>(url, body);
+    const httpOptions = {
+      headers: this.headers
+    };
+    return this.http.post<Card>(url, body, httpOptions);
   }
 
   private delete(id: string): Observable<any>{
-    return this.http.delete<any>(id);
+    const httpOptions = {
+      headers: this.headers
+    };
+    return this.http.delete<any>(id, httpOptions);
   }
 
   getAllCards(): Observable<Card[]>{
-    // const url = this.uris.getAllCards();
     return this.uris.getAllCards();
+    // return this.getAll(url);
   }
 
   getCardDetails(id: number): Observable<Card>{
-    // const url = this.uris.getCardDetails(id);
     return this.uris.getCardDetails(id);
+    // return this.get(url);
   }
 }
